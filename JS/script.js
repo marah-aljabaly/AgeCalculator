23ff1aa33c36b06f0051b75f387de03e66307f56
marah-aljabaly <aljabalymarah@gmail.com>
2024-06-19 16:55:03 +0300

    script.js

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,9 @@
 //input date
 let ageDay = document.querySelector('.js-day');
 let ageMonth = document.querySelector('.js-month');
-let ageYear = document.querySelector('.js-year'); 
-
-//important data
-yearError = document.querySelector('.year-error');
-monthError = document.querySelector('.month-error');
-dayError = document.querySelector('.day-error');
-p_year = document.querySelector('.p-year');
-p_month = document.querySelector('.p-month');
-p_day = document.querySelector('.p-day');
+let ageYear = document.querySelector('.js-year');  
 
 let month = [31,28,31,30,31,30,31,31,30,31,30,31];
-
 
 // current date
 const currentTime = new Date();
@@ -20,104 +11,146 @@ let currentDay = currentTime.getDate();
 let currentMonth = currentTime.getMonth() + 1;
 let currentYear = currentTime.getFullYear();
 
+//important data
+let yearError = document.querySelector('.year-error');
+let monthError = document.querySelector('.month-error');
+let dayError = document.querySelector('.day-error');
+let p_year = document.querySelector('.p-year');
+let p_month = document.querySelector('.p-month');
+let p_day = document.querySelector('.p-day');
+
 
 //function
 function ageCalculator() {
+
   //'the field is erquired' case
   if (ageYear.value == '' || ageMonth.value == '' || ageDay.value == '') {
     if (ageDay.value == '') {
-      ageDay.style.borderColor = "hsl(0, 100%, 67%)";
-      dayError.innerHTML = 'This field is required';
-      dayError.style.color = 'hsl(0, 100%, 67%)';
-      dayError.style.fontStyle = 'italic';
-      dayError.style.fontWeight = '500';
-      p_day.style.color = 'hsl(0, 100%, 67%)';
+      requiredField('day');
     }
     if (ageYear.value == '') {
-      ageYear.style.borderColor = "hsl(0, 100%, 67%)"; 
-      yearError.innerHTML = 'This field is required';
-      yearError.style.color = 'hsl(0, 100%, 67%)';
-      yearError.style.fontStyle = 'italic';
-      yearError.style.fontWeight = '500';
-      p_year.style.color = 'hsl(0, 100%, 67%)';
+      requiredField('month');
     }
     if (ageMonth.value == '') {
-      ageMonth.style.borderColor = "hsl(0, 100%, 67%)";
-      monthError.innerHTML = 'This field is required';
-      monthError.style.color = 'hsl(0, 100%, 67%)';
-      monthError.style.fontStyle = 'italic';
-      monthError.style.fontWeight = '500';
-      p_month.style.color = 'hsl(0, 100%, 67%)';
+      requiredField('year');
     }
   }
+
   else{
     isLeapYear(ageYear.value); //check number of day in feb. month
 
-    if (ageDay.value > month[ageMonth.value - 1] || ageDay.value < 1 || ageDay.value > currentDay) {
-      inValidDay();
-      if (ageMonth.value > 12 || ageMonth.value === 0) {
-        inValidMonth();
-      }
+    if (ageMonth.value > 12 || ageMonth.value === 0){
+      invalid('month');
+
       if (ageYear.value > currentYear) {
-        inValidYear();
+        invalid('year');
+      }
+
+      if (ageDay.value > month[ageMonth.value - 1] || ageDay.value < 1) {
+        invalid('day');
+      }
+
+      if (ageYear.value === currentYear) {
+        if (ageMonth.value > currentMonth){
+          invalid('month');
+        }
       }
     }
-    else if (ageMonth.value > 12 || ageMonth.value === 0) {
-      inValidMonth();
-      if (ageDay.value < 1 || ageDay.value > month[ageMonth.value - 1]) {
-        inValidDay();
-      }
-      if (ageYear.value > currentYear) {
-        inValidYear();
-      }
-    }
-    else if (ageYear.value > currentYear) {
-      inValidYear();
-      if (ageDay.value < 1 || ageDay.value > month[ageMonth.value - 1]) {
-        inValidDay();
-      }
-      if (ageMonth.value > 12 || ageMonth.value === 0) {
-        inValidMonth();
-      }
-    }
+
     else {
-      calculate();
+      valid('month');
+
+      if (ageDay.value > month[ageMonth.value - 1] || ageDay.value < 1) {
+        invalid('day');
+      } 
+      else {
+        valid('day');
+
+        if (ageYear.value > currentYear) {
+          invalid('year');
+        }
+        else if (ageYear.value === currentYear) {
+          if (ageMonth.value > currentMonth){
+            invalid('month');
+          }
+        }
+        else {
+          valid('year');
+          calculate();
+        }
+      }
     }
-  }  
+  } 
 }
 
 
-function inValidDay() {
-    dayError.innerHTML = "Must be a valid day";
-    dayError.style.color = 'hsl(0, 100%, 67%)';
-    dayError.style.fontStyle = 'italic';
-    dayError.style.fontWeight = '500';
-    p_day.style.color = 'hsl(0, 100%, 67%)';
-    ageDay.style.borderColor = 'hsl(0, 100%, 67%)';
+function requiredField (value) {
+  if (value === 'day') {
+    p_day.classList.add('p-name-js');
+    dayError.classList.add('error-js');
+    dayError.innerHTML = 'This field is required';
+    ageDay.classList.add('input-border-color');
+  }
+  else if (value === 'month') {
+    p_month.classList.add('p-name-js');
+    monthError.classList.add('error-js');
+    monthError.innerHTML = 'This field is required';
+    ageMonth.classList.add('input-border-color');
+  }
+  else{
+    p_year.classList.add('p-name-js');
+    yearError.classList.add('error-js');
+    yearError.innerHTML = 'This field is required';
+    ageYear.classList.add('input-border-color');
+  }
 }
 
-function inValidMonth(){
-    monthError.innerHTML = "Must be a valid month";
-    monthError.style.color = 'hsl(0, 100%, 67%)';
-    monthError.style.fontStyle = 'italic';
-    monthError.style.fontWeight = '500';
-    p_month.style.color = 'hsl(0, 100%, 67%)';
-    ageMonth.style.borderColor = 'hsl(0, 100%, 67%)';
+function invalid (value){
+  if (value === 'day') {
+    p_day.classList.add('p-name-js');
+    dayError.classList.add('error-js');
+    dayError.innerHTML = 'Must be a valid day';
+    ageDay.classList.add('input-border-color');
+  }
+  else if (value === 'month') {
+    p_month.classList.add('p-name-js');
+    monthError.classList.add('error-js');
+    monthError.innerHTML = 'Must be a valid month';
+    ageMonth.classList.add('input-border-color');
+  }
+  else {
+    p_year.classList.add('p-name-js');
+    yearError.classList.add('error-js');
+    yearError.innerHTML = 'Must be a valid year';
+    ageYear.classList.add('input-border-color');
+  }
 }
 
-
-function inValidYear() {
-    yearError.innerHTML = "Must be in the past";
-    yearError.style.color = 'hsl(0, 100%, 67%)';
-    yearError.style.fontStyle = "italic";
-    yearError.style.fontWeight = '500';
-    p_year.style.color = 'hsl(0, 100%, 67%)';
-    ageYear.style.borderColor = 'hsl(0, 100%, 67%)';
+function valid (value){
+  if (value === 'day') {
+    p_day.classList.remove('p-name-js');
+    dayError.classList.remove('error-js');
+    dayError.innerHTML = '';
+    ageDay.classList.remove('input-border-color');
+    ageDay.classList.remove('input-border-color:focus');
+  }
+  else if (value === 'month') {
+    p_month.classList.remove('p-name-js');
+    monthError.classList.remove('error-js');
+    monthError.innerHTML = '';
+    ageMonth.classList.remove('input-border-color');
+  }
+  else {
+    p_year.classList.remove('p-name-js');
+    yearError.classList.remove('error-js');
+    yearError.innerHTML = '';
+    ageYear.classList.remove('input-border-color');
+  }
 }
 
 function calculate(){
   if (ageDay.value > currentDay){
-    currentDay = currentDay + month[ageMonth - 1];
+    currentDay += month[ageMonth.value - 1];
     currentMonth = currentMonth - 1;
   }
   if (ageMonth.value > currentMonth) {  
@@ -129,6 +162,7 @@ function calculate(){
   let yearResult = currentYear - ageYear.value;
   let monthResult = currentMonth - ageMonth.value;
   let dayResult = currentDay - ageDay.value;
+
 
   // display result 
   document.querySelector('#year-result').innerHTML = yearResult;
@@ -146,6 +180,3 @@ function isLeapYear(year) {
         month[1] = 28;
     }
 }
-
-
-
